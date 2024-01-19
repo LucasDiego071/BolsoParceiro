eea01f2895d70fad1681f6de8603a9c21f0e3690
Lucas Diego Santos da Silva <149350008+LucasDiego071@users.noreply.github.com>
2024-01-19 01:04:16 -0300

    Release 1.1.0

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ import datetime
 import pandas as pd
 
 
-#===================Inputs Via app===================
+# ===================Inputs Via app===================
 
 st.image("logo.png")
 
@@ -18,11 +18,14 @@ nome = st.text_input("Insira Seu Nome:", "Joãozinho da Silva")
 
 renda = st.text_input("Insira sua rendas mensais:", "1800.00; 2000.00; 300.00")
 
-gastos = st.text_input("Insira seus gastos fixosm mensais:", "Aluguel = 980.00; Agua = 180.00; Luz = 200.00")
+gastos = st.text_input("Insira seus gastos fixosm mensais:",
+                       "Aluguel = 980.00; Agua = 180.00; Luz = 200.00")
 
-gastos02 = st.text_input("Insira seus gastos extras mensais:", "Cinema = 100.00; Roupas = 300.00")
+gastos02 = st.text_input(
+    "Insira seus gastos extras mensais:", "Cinema = 100.00; Roupas = 300.00")
 
-date = st.date_input("Insira o mês que vai ser organizado:", datetime.date(2024, 2, 1))
+date = st.date_input("Insira o mês que vai ser organizado:",
+                     datetime.date(2024, 2, 1))
 
 # ===================Tratamentos de dados===================
 perfil = user.Usuarios(nome)
@@ -68,7 +71,7 @@ gastos_totais = dados_financeiros.Gastos(gastos_fixos, gastos_extras)
 
 # ---- Dados financeiros:
 
-financas = dados_financeiros.Dados_financeiros(income , gastos_totais)
+financas = dados_financeiros.Dados_financeiros(income, gastos_totais)
 
 # ---- Mês:
 mes = mes.Mes(date, perfil, financas)
@@ -79,19 +82,19 @@ st.write(f"{mes.data}")
 
 lista01 = []
 for gastos_fixos in financas.gastos_fixos:
-    formatar = {"Descrição" : f"{gastos_fixos[0]}", "Valor": gastos_fixos[1]}
+    formatar = {"Descrição": f"{gastos_fixos[0]}", "Valor": gastos_fixos[1]}
     lista01.append(formatar)
 
 lista02 = []
 for gastos_extras in financas.gastos_extras:
-    formatar = {"Descrição" : f"{gastos_extras[0]}", "Valor": gastos_extras[1]}
+    formatar = {"Descrição": f"{gastos_extras[0]}", "Valor": gastos_extras[1]}
     lista02.append(formatar)
 
 # Convertendo as listas para DataFrames do pandas
 df_gastos_fixos = pd.DataFrame(lista01)
 df_gastos_extras = pd.DataFrame(lista02)
 
-#calculando os valores:
+# calculando os valores:
 renda01 = financas.calcular_RendaTotal()
 gastos01 = financas.calcular_gastos_fixos()
 gastos02 = financas.calcular_gastos_extras()
@@ -100,8 +103,12 @@ resto01 = renda01 - gastos01
 st.write(f"Seu mês inicia com: R${renda01}")
 st.write("Gatos Fixos:")
 st.dataframe(df_gastos_fixos)
-st.write(f"Após o uso para pagar seus gastos fixos restam: R${renda01 - gastos01}")
+st.write(f"Após o uso para pagar seus gastos fixos restam: R${
+         renda01 - gastos01}")
 st.write(f"Você pode distribuir esse valor entre os gastos extras abaixo.")
 st.write("Gastos_Extras:")
 st.dataframe(df_gastos_extras)
-st.write(f"Caso você gaste o restante com todos os gastos extras, o restante do dinheiro sera: R${resto01 - gastos02}")
+st.write(f"Caso você gaste o restante com todos os gastos extras, o restante do dinheiro sera: R${
+         resto01 - gastos02}")
+
+# Feito Por Lucas Diego
