acc5079955d7dfbf77e03e2ac8f5d2b4662647d4
Lucas Diego Santos da Silva <149350008+LucasDiego071@users.noreply.github.com>
2024-01-18 22:50:26 -0300

    Release 0.1.4

--- data/interface.py
+++ data/interface.py
@@ -1,5 +1,5 @@
-import data.dados_financeiros as data_money
-import data.mes as month
+'''import data.dados_financeiros as data_money
+import data.mes as month'''
 import data.usuario as user
 import streamlit as st
 
@@ -10,7 +10,7 @@ st.title("Bolso :blue[Parceiro]")
 
 st.text("Bem vindo ao Bolso parceiro, Aqui temos a intenção de Organizar suas finanças!")
 
-nome = st.text_input()
+nome = st.text_input("Insira Seu Nome:", "João Santos da Silva")
 
 perfil = user.Usuarios(nome)
 
